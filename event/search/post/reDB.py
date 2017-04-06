3856378fbdc42dcbe28fd929730df29a5b0d020f
Jeremy Yang <jeremy2@uci.edu>
2017-04-05 22:46:42 -0700

    success dynamic search

--- event/search/post/reDB.py
+++ event/search/post/reDB.py
@@ -1,15 +1,27 @@
 import rethinkdb as r
 import json
 
-def lambda_handler(event, context):
-    conn=r.connect(host='ec2-52-33-6-252.us-west-2.compute.amazonaws.com',port=32772)
-    conn.use('mvp1')
-    print(event)
-    query=r.table('kaamloEvent').insert({
-        'id':r.uuid(),
-        'name':event['name'],
-        'location':event['location']
-        },return_changes=True)
-    newId='"'+str(query.run(conn)['changes'][0]['new_val']['id'])+'"'
-    result='{"id":'+newId+"}"
-    return json.loads(str(result))
+##def lambda_handler(event, context):
+conn=r.connect(host='ec2-52-33-6-252.us-west-2.compute.amazonaws.com',port=32772)
+conn.use('mvp1')
+print(r.table('kaamloEvent').run(conn))
+x={
+"name":"Jeremy","location":"HB"
+}
+
+
+
+
+
+def test():
+    condition = None
+    for var in x:
+        if(type(var)== str or type(var)==int):
+            conditionForThisKey=r.row[var].eq(x[var])
+        if(condition == None):
+            condition=conditionForThisKey
+        else:
+            condition = condition.and_(conditionForThisKey)
+    
+    return condition
+print(r.table('kaamloEvent').filter(test()).run(conn))
