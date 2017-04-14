0b5f4cfb83682b6c993f021a15a864f888f5ce12
madhavgharmalkar <gharmalkar.madhav@gmail.com>
2017-04-14 16:06:45 -0700

    switched to table login

--- login/post/login.py
+++ login/post/login.py
@@ -1,22 +1,71 @@
-from aws_srp import AWSSRP
-import os
+import uuid
+import jwt
+import json
+import time
+
+from passlib.hash import pbkdf2_sha256
+import pymysql.cursors
+
+# grab our jwt secret wich will be the word secret for now
+jwt_secret = 'secret'
 
 def login_handler(event, context):
+
     username = event['username']
     password = event['password']
 
-    client_id = os.environ['client_id']
-    pool_id = os.environ['pool_id']
-
+    # connect to the database to fetch login info
+    connection = pymysql.connect(
+        host="mvp1-kaamlo-logintable.cczywhkujcku.us-west-2.rds.amazonaws.com",
+        user="kaamlo",
+        password="Radhe108!",
+        db="kaamlo",
+        charset='utf8mb4',
+        port=3306,
+        connect_timeout=5,
+        cursorclass=pymysql.cursors.DictCursor)
 
     try:
-        loginClient = AWSSRP(username, password, pool_id, client_id)
-        authAttempt = loginClient.authenticate_user()
 
-        return {
-            'ExpiresIn': authAttempt['AuthenticationResult']['ExpiresIn'],
-            'IdToken': authAttempt['AuthenticationResult']['IdToken']
-        }
+        with connection.cursor() as cursor:
+
+            # create a tuple of values to search for in the db
+            values = (username, username)
+
+            # retrieve user info
+            cursor.execute("""SELECT * FROM `kaamlo_login` where email = %s or username = %s""", values)
+            result = cursor.fetchone()
+
+            # pasword validation
+            if pbkdf2_sha256.verify(password, result['password']):
+                # calculate the expiration time of this token
+                expiresAt = time.time() + 3600
+
+                # create the JSON payload to return
+                jwt_payload = {
+                    'userId': result['user_id'],
+                    'exp': expiresAt
+                }
+
+                # create the jwt
+                jwt_token = jwt.encode(jwt_payload, jwt_secret, algorithm='HS256')
+
+                # return the token
+                return {
+                    'token': jwt_token,
+                    'expiresIn': 3600
+                }
+            else:
+                raise_error(401, "Bad Login")
+
 
     except Exception as e:
-        print(e)
+        raise_error(500, 'Internal server error')
+
+def raise_error(code, message):
+    error = {
+        'httpStatus': code,
+        'message': message
+    }
+
+    raise Exception(json.dumps(error))
