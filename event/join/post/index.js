4a54c6c9da5b159e2689234e882a6f1415d3e542
hpmaharaja <heranp@uci.edu>
2017-05-01 18:05:40 -0700

    added and completed join event

--- event/join/post/index.js
+++ event/join/post/index.js
@@ -1,27 +1,29 @@
 'use strict';
 
-var AWS = require("aws-sdk");
-AWS.config.update({ region: "us-west-2" });
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'kaamlo-db.cczywhkujcku.us-west-2.rds.amazonaws.com',
+  user     : 'hpmaharaja',
+  password : 'Radhe108!',
+  database : 'kaamlo_db'
+});
 
-const tableName = "fullAttendeeInfo";
+connection.connect();
 
 exports.handler = (event, context, callback) => {
 
-    let docClient = new AWS.DynamoDB.DocumentClient();
-    let params = {
-        TableName: tableName,
-        Item: {
-            "eventId": event.eventID,
-            "userId": event.userId
-        }
-    };
-
-    docClient.put(params, function(err, data) {
-        if (err) {
-            callback(err);
-        } else {
-            callback(null, JSON.stringify({msg: "Successfully updated fullAttendeeInfo Table!"}));
-        }
+    connection.query('INSERT INTO attendeeInfo (eventId, userId) VALUES(?,?)',[ event.eventId, event.userId ], function(err, rows, fields) {
+      if (!err) {
+        // console.log('The solution is: ', rows);
+        console.log('Successfully added attendeeInfo');
+        callback(null, JSON.stringify({msg: "Successfully added from attendeeInfo Table!", eventId: event.eventId, userId: event.userId}));
+      } else {
+        console.log('Error while performing Query.');
+        callback(null, JSON.stringify(err));
+      }
     });
+
+    connection.end();
+
 
 };
